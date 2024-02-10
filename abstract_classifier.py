95db164a53397d9558205e26e984bd9e5f1f6a98
Shakeel Ahmad <shakhd@duck.com>
2024-02-10 14:46:35 +0100

    generate csv with classification, stability and robustness results

--- abstract_classifier.py
+++ abstract_classifier.py
@@ -1,15 +1,16 @@
 from collections import defaultdict
+import csv
 from dataclasses import dataclass
 from typing import Optional
-
 from sklearn.metrics import DistanceMetric
-from src.utils.base_types import Label
 from sys import argv
 from os.path import exists, join
+from os import mkdir
 from tqdm import tqdm
 from numpy import zeros
 import logging
 import argparse
+import time
 
 from src.abstract.classifier import AbstractClassifier
 from src.dataset.loader import DataLoader
@@ -18,13 +19,30 @@ from src.utils.configuration import Configuration
 
 logger = logging.getLogger(__name__)
 
+def get_args_parser() -> argparse.ArgumentParser:
+
+  parser = argparse.ArgumentParser(description='Abstract Knn')
+  parser.add_argument('config_filepath', metavar='CONFIGFILE',
+                      help='name of the config file to read (it must be inside the config folder !!).')
+  parser.add_argument('--random-state', metavar='RANDOM', type=int,
+                      help='random state used when partitioning the dataset.')
+  parser.add_argument('--partition-size', metavar='SIZE', type=int,
+                      default=20,
+                      help='maximum number of data points in a partition (default 20).')
+  parser.add_argument('--log ', dest='log_level',
+                      choices=['INFO', 'DEBUG', 'ERROR'],
+                      default='ERROR',
+                      help='log level used during the verification phase (default ERROR).')
+  parser.add_argument('--results-dir', metavar='RESULTSDIR',
+                      default='./results',
+                      help='path to a directory where results will be saved (default ./results)')
+
+  return parser
+
+
 def main(params: Configuration, partition_size: int = 20,
-         random_state: Optional[int] = None) -> None:
-  @dataclass
-  class Result:
-    is_stable: bool
-    is_robust: bool
-    classification: set[Label]
+         random_state: Optional[int] = None,
+         results_dir: str = './results') -> None:
 
   k_values = params['knn_params']['k_values']
 
@@ -32,7 +50,6 @@ def main(params: Configuration, partition_size: int = 20,
   robust_count = zeros(len(k_values))
 
   loader = DataLoader(params['dataset'])
-  results: defaultdict[int, list[Result]] = defaultdict(list)
 
   training_set, test_set = loader.load_datasets(params['base_dirs']['dataset_dir'])
 
@@ -50,6 +67,13 @@ def main(params: Configuration, partition_size: int = 20,
   epsilon = params['abstraction']['epsilon']
 
   classified_points = 0
+  classification_results: list[list[str]]  = []
+  robustness_results: list[list[str]]      = []
+  stability_results: list[list[str]]       = []
+
+  clock_st: float = time.time()
+  process_st: float = time.process_time()
+
   for (test_point, test_label) in progress_bar:
     logger.info("-- Classifying point %s %s with label %s --\n", classified_points + 1 , test_point, test_label)
     adv_region = AdversarialRegion(test_point, epsilon, test_set.num_feature_start_ix)
@@ -58,20 +82,30 @@ def main(params: Configuration, partition_size: int = 20,
 
     labels = abstract_classifier.get_classification(adv_region, k_values)
 
-    logger.info('\tlabels: ')
-    for k, item in labels.items():
-      logger.info(f'\t\tk = {k} -> {item}', )
-    logger.info('\n')
+    classification_result: list[str]  = [str(classified_points+1), str(test_label)]
+    robustness_result: list[str]      = [str(classified_points+1)]
+    stability_result: list[str]       = [str(classified_points+1)]
 
+    logger.info('\tlabels: ')
     for ix, (k, classification) in enumerate(labels.items()):
+
+      logger.info(f'\t\tk = {k} -> {classification}', )
+      classification_result.append(str(classification))
 
       is_stable = len(classification) == 1
       is_robust = is_stable and classification.pop() == test_label
 
-      results[k].append(Result(is_stable, is_robust, classification))
+      robustness_result.append('Yes' if is_robust else 'No')
+      stability_result.append('Yes' if is_stable else 'No')
 
       stable_count[ix] += is_stable
       robust_count[ix] += is_robust
+
+    logger.info('\n')
+
+    classification_results.append(classification_result)
+    robustness_results.append(robustness_result)
+    stability_results.append(stability_result)
 
     classified_points += 1
     progress_bar.set_postfix_str('ROB={}%, STAB={}%'.format(
@@ -81,40 +115,68 @@ def main(params: Configuration, partition_size: int = 20,
 
   logger.info(f"-- Finished verifying point {classified_points} --\n")
 
-  logger.info("\t Provable stability percentages :")
-  logger.info('\t\t\n'.join([f'{k}: {robust_points/classified_points}' for robust_points, k in zip(robust_count, k_values)]))
+  elapsed_clock: float = time.time() - clock_st
+  elapsed_process: float = time.process_time() - process_st
+
+  overall_results: list[list[str]] = [['Overall stability percentage'] +\
+      [f'{(stable_points/classified_points)*100}' for stable_points in stable_count]]
+
+  overall_results.append(['Overall robustness percentage'] +\
+      [f'{(robust_points/classified_points)*100}' for robust_points in robust_count])
+
+  overall_results.append(['']*len(k_values))
+  overall_results.append(['runtime (clock time)', f'{time.strftime("%H:%M:%S", time.gmtime(elapsed_clock))}']+\
+                          ['']*(len(k_values)-1))
+  overall_results.append(['runtime (process time)', f'{time.strftime("%H:%M:%S", time.gmtime(elapsed_process))}']+\
+                          ['']*(len(k_values)-1))
+
+  with open(join(results_dir, 'classification.csv'), 'w', newline='', encoding='utf-8') as classification_file,\
+       open(join(results_dir, 'robustness.csv'), 'w', newline='', encoding='utf-8') as robustness_file,\
+       open(join(results_dir, 'stability.csv'), 'w', newline='', encoding='utf-8') as stability_file,\
+       open(join(results_dir, 'overall_result.csv'), 'w', newline='', encoding='utf-8') as overall_result_file:
+
+    classification_writer = csv.writer(classification_file)
+    classification_header = ['Test point #', 'Actual Classification'] + [f' Classification at K={k}' for k in k_values]
+    classification_writer.writerow(classification_header)
+
+    robustness_writer = csv.writer(robustness_file)
+    robustness_header = ['Test point #'] + [f'Robustness at K={k}' for k in k_values]
+    robustness_writer.writerow(robustness_header)
+
+    stability_writer = csv.writer(stability_file)
+    stability_header = ['Test point #'] + [f'Stability at K={k}' for k in k_values]
+    stability_writer.writerow(stability_header)
+
+    overall_result_writer = csv.writer(overall_result_file)
+    overall_result_header = ['At'] + [f'K={k}' for k in k_values]
+    overall_result_writer.writerow(overall_result_header)
+
+    classification_writer.writerows(classification_results)
+    robustness_writer.writerows(robustness_results)
+    stability_writer.writerows(stability_results)
+    overall_result_writer.writerows(overall_results)
 
 if __name__ == "__main__":
 
   try:
 
-    parser = argparse.ArgumentParser(description='Abstract Knn')
-    parser.add_argument('configFilepath', metavar='CONFIGFILE',
-                        help='name of the config file to read (it must be inside the config folder !!).')
-    parser.add_argument('--random-state', metavar='RANDOM', type=int,
-                        help='random state used when partitioning the dataset.')
-    parser.add_argument('--partition-size', metavar='SIZE', type=int,
-                        default=20,
-                        help='max size of the data points in a partition (default 20).')
-    parser.add_argument('--log ', dest='log_level',
-                        choices=['INFO', 'DEBUG', 'ERROR'],
-                        default='ERROR',
-                        help='log level used during the verification phase (default ERROR).')
-
-    args = parser.parse_args()
+    args = get_args_parser().parse_args()
 
     params = Configuration()
 
     logging.basicConfig(filename='logs.log', filemode="w", level=args.log_level.upper(),
                         format="%(message)s")
 
-    input_file_path = join(params['base_dirs']['config'], f'{argv[1]}.toml')
+    input_file_path = join(params['base_dirs']['config'], f'{args.config_filepath}.toml')
     if not exists(input_file_path):
       raise ValueError(f"Input file {input_file_path} not found:")
 
     params.load_settings(input_file_path)
 
-    main(params, args.partition_size, args.random_state)
+    if not exists(args.results_dir):
+      mkdir(args.results_dir)
+
+    main(params, args.partition_size, args.random_state, args.results_dir)
 
   except Exception:
     logger.exception('An error occurred: ')
